41d803062f721a3a41b40288e4d1c1ffed8e78bf
Boyd <mdboyd@outlook.com>
2021-09-18 19:50:12 -0400

    Updates

--- bootstrap-nginx.sh
+++ bootstrap-nginx.sh
@@ -1,26 +1,15 @@
-#!/bin/bash -xe
+#!/bin/bash
+
 set -x
 
-sudo amazon-linux-extras install nginx1.12 -y
-sudo yum install openssl httpd-tools -y
-
-# Create a self-signed cert
-sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/certs/nginx.key -out /etc/ssl/certs/nginx.crt -subj "/CN=localhost"
+sudo yum install httpd-tools -y
 
 # Generate a dummy password
 echo livy | sudo htpasswd -ci /etc/nginx/.htpasswd livy
 
-## Starting Nginx Services
-sudo chkconfig nginx on
-sudo service nginx start
-
 ## Modifying Nginx Server Configuration
 cat > /tmp/nginx.conf <<EOL
-user nginx;
-worker_processes auto;
-# include /usr/share/nginx/modules/*.conf;
-events { }
-http {
+
 
     server {
         listen 80;
@@ -40,22 +29,20 @@ http {
         }        
     }
     server {
-        listen 443;
-        listen [::]:443;
+        listen 443 ssl;
 
         auth_basic "";
         auth_basic_user_file /etc/nginx/.htpasswd;
 
-         # Certificate
-         ssl_certificate /etc/ssl/certs/nginx.crt;
-         # Private Key
-         ssl_certificate_key /etc/ssl/certs/nginx.key;    
+     
+        ssl_certificate /etc/ssl/certs/nginx.crt;
+        ssl_certificate_key /etc/ssl/certs/nginx.key;
 
-         ssl on;
-         ssl_session_cache  builtin:1000  shared:SSL:10m;
-         ssl_protocols  TLSv1 TLSv1.1 TLSv1.2;
-         ssl_ciphers HIGH:!aNULL:!eNULL:!EXPORT:!CAMELLIA:!DES:!MD5:!PSK:!RC4;
-         ssl_prefer_server_ciphers on;          
+
+        
+        ssl_session_cache  builtin:1000  shared:SSL:10m;
+        ssl_protocols  TLSv1.2;
+
         location / {
             proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
             proxy_set_header X-Real-IP \$remote_addr;
@@ -68,9 +55,12 @@ http {
             proxy_read_timeout 240s;
         }
     }
-}
+
 EOL
 
-sudo cp /tmp/nginx.conf /etc/nginx/nginx.conf
+sudo cp /tmp/nginx.conf /etc/nginx/conf.d/livyproxy.conf
+
+#Add an include to the http directive
+sed -i 's/http {/http { \n    #CUSTOM INCLUDES \n    include \/etc\/nginx\/conf.d\/*.conf;\n    #END CUSTOM INCLUDES/' nginx.conf
 
 sudo service nginx restart
